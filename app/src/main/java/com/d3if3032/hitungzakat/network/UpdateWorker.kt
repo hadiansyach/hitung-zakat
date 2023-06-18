10c9c127b120fce62093257259cf380f6b5fe2aa
Ihsan Hadiansyach <ihadiansyach@gmail.com>
2023-06-18 07:23:18 +0700

    membuat notifikasi updater

--- app/src/main/java/com/d3if3032/hitungzakat/network/UpdateWorker.kt
+++ app/src/main/java/com/d3if3032/hitungzakat/network/UpdateWorker.kt
@@ -1,18 +1,26 @@
 package com.d3if3032.hitungzakat.network
 
 import android.Manifest
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.d3if3032.hitungzakat.MainActivity
+import com.d3if3032.hitungzakat.R
 
 class UpdateWorker(context: Context, workerParams: WorkerParameters) :
     CoroutineWorker(context, workerParams) {
 
     companion object {
         const val WORK_NAME = "updater"
+        private const val NOTIFICATION_ID = 44
     }
 
     override suspend fun doWork(): Result {
@@ -25,6 +33,32 @@ class UpdateWorker(context: Context, workerParams: WorkerParameters) :
             return Result.failure()
         }
         Log.d("Worker", "Menjalankan proses background..")
+
+        val builder = NotificationCompat.Builder(applicationContext,
+            MainActivity.CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(applicationContext.getString(
+                R.string.notif_title))
+            .setContentText(applicationContext.getString(
+                R.string.notif_text))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(getPendingIntent(applicationContext))
+            .setAutoCancel(true)
+        val manager = NotificationManagerCompat.from(applicationContext)
+        manager.notify(NOTIFICATION_ID, builder.build())
+
         return Result.success()
     }
+
+    private fun getPendingIntent(context: Context): PendingIntent {
+        val intent = Intent(context, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                Intent.FLAG_ACTIVITY_CLEAR_TASK
+        var flags = PendingIntent.FLAG_UPDATE_CURRENT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            flags = flags or PendingIntent.FLAG_IMMUTABLE
+        }
+        return PendingIntent.getActivity(context, 0, intent, flags)
+    }
+
 }
