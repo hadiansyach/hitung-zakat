0ebe4448124b90229ce2de71ff5a343f168ba569
Ihsan Hadiansyach <ihadiansyach@gmail.com>
2023-06-18 07:10:36 +0700

    membuat channel notifikasi

--- app/src/main/java/com/d3if3032/hitungzakat/MainActivity.kt
+++ app/src/main/java/com/d3if3032/hitungzakat/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.d3if3032.hitungzakat
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
@@ -7,6 +11,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val CHANNEL_ID = "updater"
+    }
 
     private lateinit var navController: NavController
 
@@ -14,9 +21,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.channel_desc)
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager?
+            manager?.createNotificationChannel(channel)
+        }
+
         navController = findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()
     }
